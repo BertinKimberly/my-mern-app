88fd9ba8b43ecb6e7059ba08e002841b27835e59
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-08-24 13:19:59 +0200

    added sidebar

--- frontend/src/pages/userpage/Userpage.jsx
+++ frontend/src/pages/userpage/Userpage.jsx
@@ -1,17 +1,130 @@
 import React, { useEffect, useState } from "react";
 import "./userpage.css";
 import { useNavigate } from "react-router-dom";
+import {
+   FaBars,
+   FaBell,
+   FaHome,
+   FaSearch,
+   FaUpload,
+   FaVideo,
+   FaVideoSlash,
+   FaYoutubeSquare,
+} from "react-icons/fa";
 
 const Userpage = () => {
    const navigate = useNavigate();
    const [currentUser, setCurrentUser] = useState("");
+   const [showMenu, setShowMenu] = useState(false);
 
    useEffect(() => {
       if (!localStorage.getItem("current-user")) {
          navigate("/login");
       } else setCurrentUser(JSON.parse(localStorage.getItem("current-user")));
    }, []);
-   return <div>Hello {currentUser}</div>;
+   return (
+      <div>
+         <nav>
+            <div className='container nav__container'>
+               <div className='left-side'>
+                  <FaBars onClick={() => setShowMenu((prev) => !prev)} />
+                  <div className='logo'>BRT</div>
+               </div>
+
+               <div className='searchbar'>
+                  <input type='text' />
+                  <div className='srch'>
+                     <FaSearch color='white' />
+                  </div>
+               </div>
+               <div className='right-side'>
+                  <div className='upload'>
+                     <FaUpload />
+                  </div>
+                  <div className='notifications'>
+                     <FaBell />
+                  </div>
+                  <div className='user'>I</div>
+               </div>
+            </div>
+         </nav>
+         {showMenu ? (
+            <div className='sidebar'>
+               <div>
+                  <FaHome />
+                  <small>Home</small>
+               </div>
+               <div>
+                  <FaVideo />
+                  <small>Reels</small>
+               </div>
+               <div>
+                  <FaYoutubeSquare />
+                  <small>Videos</small>
+               </div>
+            </div>
+         ) : (
+            <div className="detailled-sidebar">
+                <section>
+                <div>
+                  <FaHome />
+                  <small>Home</small>
+               </div>
+               <div>
+                  <FaVideo />
+                  <small>Reels</small>
+               </div>
+               <div>
+                  <FaYoutubeSquare />
+                  <small>Videos</small>
+               </div>
+                </section>
+                <section>
+                <div>
+                  <FaHome />
+                  <small>Home</small>
+               </div>
+               <div>
+                  <FaVideo />
+                  <small>Reels</small>
+               </div>
+               <div>
+                  <FaYoutubeSquare />
+                  <small>Videos</small>
+               </div>
+                </section>
+                <section>
+                <div>
+                  <FaHome />
+                  <small>Home</small>
+               </div>
+               <div>
+                  <FaVideo />
+                  <small>Reels</small>
+               </div>
+               <div>
+                  <FaYoutubeSquare />
+                  <small>Videos</small>
+               </div>
+                </section>
+                <section>
+                <div>
+                  <FaHome />
+                  <small>Home</small>
+               </div>
+               <div>
+                  <FaVideo />
+                  <small>Reels</small>
+               </div>
+               <div>
+                  <FaYoutubeSquare />
+                  <small>Videos</small>
+               </div>
+                </section>
+            </div>
+         )}
+      </div>
+   );
 };
 
 export default Userpage;
